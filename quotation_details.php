d3c2a9c511283039d58a72cb7da491af66639637
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-01-10 17:36:28 +0530

    Update quotation_details.php

--- quotation_details.php
+++ quotation_details.php
@@ -70,40 +70,5 @@
     <!-- Internet Connection Status-->
     <div class="internet-connection-status" id="internetStatus"></div>
     <!-- Footer Nav-->
-    <div class="footer-nav-area" id="footerNav">
-      <div class="container h-100 px-0">
-        <div class="suha-footer-nav h-100">
-          <ul class="h-100 d-flex align-items-center justify-content-between ps-0">
-            <li class="active"><a href="home.php"><i class="lni lni-home"></i>Home</a></li>
-            <li><a href="message.php"><i class="lni lni-life-ring"></i>Support</a></li>
-            <li><a href="cart.php"><i class="lni lni-shopping-basket"></i>Cart</a></li>
-            <li><a href="pages.php"><i class="lni lni-heart"></i>Pages</a></li>
-            <li><a href="settings.php"><i class="lni lni-cog"></i>Settings</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <!-- All JavaScript Files-->
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/jquery.countdown.min.js"></script>
-    <script src="js/jquery.passwordstrength.js"></script>
-    <script src="js/dark-mode-switch.js"></script>
-    <script src="js/no-internet.js"></script>
-    <script src="js/active.js"></script>
-    <script src="js/pwa.js"></script>
-    <script type="text/javascript">
-      $('#comment').click(function(e){
-         swal("Good job!", " RECEIVE THE QUOTATION WITHIN 72 HOURS..!", "success");
-       });
-    </script>
-  </body>
-
-<!-- Mirrored from designing-world.com/suha-v2.5.0/notification-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Sep 2021 12:08:45 GMT -->
-</html>
+     <?php include 'footer.php';?>
+     <?php include 'footer-link.php';?>
