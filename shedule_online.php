e448168ed1c1d9ed4c9e72695bb9130ef96a837e
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-01-12 13:39:38 +0530

    Update shedule_online.php

--- shedule_online.php
+++ shedule_online.php
@@ -16,44 +16,6 @@
         <div class="suha-navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#suhaOffcanvas" aria-controls="suhaOffcanvas"><span></span><span></span><span></span></div>
       </div>
     </div>
-    <div class="offcanvas offcanvas-start suha-offcanvas-wrap" tabindex="-1" id="suhaOffcanvas" aria-labelledby="suhaOffcanvasLabel">
-      <!-- Close button-->
-      <button class="btn-close btn-close-white text-reset" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      <!-- Offcanvas body-->
-      <div class="offcanvas-body">
-        <!-- Sidenav Profile-->
-        <div class="sidenav-profile">
-          <div class="user-profile"><img src="img/bg-img/9.jpg" alt=""></div>
-          <div class="user-info">
-            <h6 class="user-name mb-1">Suha Sarah</h6>
-            <p class="available-balance">Total balance $<span class="counter">583.67</span></p>
-          </div>
-        </div>
-        <!-- Sidenav Nav-->
-        <ul class="sidenav-nav ps-0">
-          <li><a href="profile.html"><i class="lni lni-user"></i>My Profile</a></li>
-          <li><a href="notifications.html"><i class="lni lni-alarm lni-tada-effect"></i>Notifications<span class="ms-3 badge badge-warning">3</span></a></li>
-          <li class="suha-dropdown-menu"><a href="#"><i class="lni lni-cart"></i>Shop Pages</a>
-            <ul>
-              <li><a href="shop-grid.html">- Shop Grid</a></li>
-              <li><a href="shop-list.html">- Shop List</a></li>
-              <li><a href="single-product.html">- Product Details</a></li>
-              <li><a href="featured-products.html">- Featured Products</a></li>
-              <li><a href="flash-sale.html">- Flash Sale</a></li>
-            </ul>
-          </li>
-          <li><a href="pages.html"><i class="lni lni-empty-file"></i>All Pages</a></li>
-          <li class="suha-dropdown-menu"><a href="wishlist-grid.html"><i class="lni lni-heart"></i>My Wishlist</a>
-            <ul>
-              <li><a href="wishlist-grid.html">- Wishlist Grid</a></li>
-              <li><a href="wishlist-list.html">- Wishlist List</a></li>
-            </ul>
-          </li>
-          <li><a href="settings.html"><i class="lni lni-cog"></i>Settings</a></li>
-          <li><a href="intro.html"><i class="lni lni-power-switch"></i>Sign Out</a></li>
-        </ul>
-      </div>
-    </div>
     <div class="page-content-wrapper">
       <div class="container marg">
         <!-- Profile Wrapper-->
@@ -71,32 +33,29 @@
           <!-- User Meta Data-->
           <div class="card user-data-card">
             <div class="card-body">
-              <table class="table" style="font-size: 10px; border: 1px;">
+              <table class="table table-bordered text-center" style="font-size: 10px; border: 1px;">
                 <thead>
                   <tr>
                     <td>1</td>
                     <td>DETAILS OF ITEMS TO BE ASSESSED</td>
-                    <td></td>
-                    <td>SG- SINGLE GIRDER DG- DOUBLE GIRDER P/M- PILLAR MOUNTED C/M- COLUMN MOUNTED EWRH- ELECTRIC WIRE ROPE HOIST
+                    
+                    <td colspan="2">SG- SINGLE GIRDER DG- DOUBLE GIRDER P/M- PILLAR MOUNTED C/M- COLUMN MOUNTED EWRH- ELECTRIC WIRE ROPE HOIST
                     </td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>QTY. (IN NOS.)</td>
-                    <td></td>
-                    <td></td>
+                    <td colspan="2"></td>
                 </tr>
                 <tr>
                     <td>3</td>
                     <td>NOS. OF DAYS REQUIRED FOR ASSESSMENT/HEALTH CHECK-UP</td>
-                    <td></td>
-                    <td></td>
+                    <td colspan="2"></td>
                 </tr>
                  <tr>
                     <td>4</td>
                     <td>Total ASSESSMENT CHARGES @ INR 7500.00 + GST @ 18% AT PER MAN PER DAY BASIS</td>
-                    <td>INR 0.00</td>
-                    <td></td>
+                    <td colspan="2">INR 0.00</td>
                 </tr>
                  <tr>
                     <td>5</td>
