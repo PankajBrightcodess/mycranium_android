27ff5f5f439089596df2f2ad88b3474478d4e55f
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-01-15 13:47:43 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$conn=new mysqli("localhost","root","","db_mycranium");
 }
 else{
-	$conn=new MySQLi("localhost","db_mycranium","Shivanya@123#","db_mycranium");
+	$conn=new MySQLi("localhost","softwarebss_crenium","Crenium@2021","softwarebss_crenium");
 }
 if($conn->connect_errno){
 	echo "connection failed";
