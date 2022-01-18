a311e45d584392cd0d72553b02d141718df43a9f
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-01-18 17:40:31 +0530

    Update quotation_details.php

--- quotation_details.php
+++ quotation_details.php
@@ -70,26 +70,33 @@
     <div class="internet-connection-status" id="internetStatus"></div>
     <!-- Footer Nav-->
      <?php include 'footer.php';?>
-     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/jquery.countdown.min.js"></script>
-    <script src="js/jquery.passwordstrength.js"></script>
-    <script src="js/dark-mode-switch.js"></script>
-    <script src="js/no-internet.js"></script>
-    <script src="js/active.js"></script>
-    <script src="js/pwa.js"></script>
-    <script type="text/javascript">
+     <script type="text/javascript">
       $('#comment').click(function(e){
          swal("Good job!", " RECEIVE THE QUOTATION WITHIN 72 HOURS..!", "success");
        });
     </script>
+       <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.passwordstrength.js"></script>
+    <script src="js/dark-mode-switch.js"></script>
+    <script src="js/active.js"></script>
+    <script src="js/pwa.js"></script>
+    <script src="js/switchery.js"></script>
+    <script type="text/javascript">
+       $(document).ready(function() {
+        $('.multipleselect').select2();
+       });
+     </script>
   </body>
 
-<!-- Mirrored from designing-world.com/suha-v2.5.0/notification-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Sep 2021 12:08:45 GMT -->
+<!-- Mirrored from designing-world.com/suha-v2.5.0/intro.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Sep 2021 12:08:13 GMT -->
 </html>
