65de389fd978daedc3179a81be916b4f58ba1564
PankajBrightcodess <86092953+PankajBrightcodess@users.noreply.github.com>
2022-02-20 00:20:47 +0530

    update

--- connection.php
+++ connection.php
@@ -2,6 +2,9 @@
 if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$conn=new mysqli("localhost","root","","db_mycranium");
 }
+elseif(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost:8080'){
+	$conn=new mysqli("localhost","root","","db_mycranium");
+}
 else{
 	$conn=new MySQLi("localhost","softwarebss_crenium","Crenium@2021","softwarebss_crenium");
 }
