b1694f6822e45dd18ef027584197c9256f346c95
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-03-11 17:26:24 +0530

    Update payment_constant.php

--- payment_constant.php
+++ payment_constant.php
@@ -1,6 +1,6 @@
 <?php
-	define("API_KEY","rzp_test_SSfDzVEzwPPG2U");
-	// define("API_KEY","rzp_live_17JhShzEF7UdzO");
+	// define("API_KEY","rzp_test_SSfDzVEzwPPG2U");
+	define("API_KEY","rzp_live_17JhShzEF7UdzO");
 	// define("API_KEY","rzp_live_ESJiSGIEkrg68p");
 	// define("API_KEY","rzp_test_KVV2yNPLssjS3jUvH171bc3x");
 	
