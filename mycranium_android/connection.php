aaea499748bb991a90ebf43a8793a1cdcd29bfca
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-03-23 17:24:11 +0530

    Update connection.php

--- mycranium_android/connection.php
+++ mycranium_android/connection.php
@@ -6,7 +6,7 @@ elseif(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost:8080'){
 	$conn=new mysqli("localhost","root","","db_mycranium");
 }
 else{
-	$conn=new MySQLi("localhost","bssrauoj_craniumdb","Craniumdb@2022","bssrauoj_craniumdb");
+	$conn=new MySQLi("localhost","softwarebss_crenium","Crenium@2021","softwarebss_crenium");
 }
 if($conn->connect_errno){
 	echo "connection failed";
